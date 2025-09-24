1cc4489ae00940189ddc35495a3e260f05b7d84a
LeeKM321 <stephen4951@gmail.com>
2025-09-24 13:04:11 +0900

    객체지향 5대개념 SOLID - ISP

---
+++ src/oop/solid/isp/SmartSpeaker.java
@@ -0,0 +1,19 @@
+package oop.solid.isp;
+
+public class SmartSpeaker implements PowerControllable, MusicPlayable {
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+
+    @Override
+    public void playMusic() {
+
+    }
+
+}
