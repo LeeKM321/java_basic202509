82e26acc365962435767a1ef4bd9ef531b32bfec
LeeKM321 <stephen4951@gmail.com>
2025-10-14 14:45:02 +0900

    제네릭의 다양한 문법

---
+++ src/etc/generic/bad/Box.java
@@ -0,0 +1,14 @@
+package etc.generic.bad;
+
+public class Box {
+
+    private Object obj;
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+}
---
+++ src/etc/generic/good/Box.java
@@ -0,0 +1,14 @@
+package etc.generic.good;
+
+public class Box<T> {
+
+    private T obj;
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+}
